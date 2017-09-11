5629d72b13dd8dd9ff6acb15a2fe97be26fd09de
Kenneth Vo <kennyvo91@gmail.com>
2017-09-11 10:54:54 -0500

    removed banner header

--- controllers/bidController.js
+++ controllers/bidController.js
@@ -46,50 +46,6 @@ function create(req, res) {
     })
   });
 };
-// function create(req, res) {
-//   db.Listing.findById(req.params.listingId, function(err, foundListing) {
-//     // console.log(foundListing._id );
-//     if (err) {
-//       console.log('Error', err);
-//     }
-//     // res.json(foundListing);
-//     // get array of all users
-//     db.User.find({}, function(err, users) {
-//       // for loop iterates through all users' listings
-//       for (let i = 0; i < users.length; i++) {
-//         let listings = users[i].listings
-//         // for loop iterates through all listings ids
-//         for (let j = 0; j < listings.length; j++) {
-//           // finds match
-//           // comparing _id with _id returning false. Not sure why, will return later
-//           if (listings[j].topic === foundListing.topic && listings[j].created === foundListing.created) {
-//             console.log("Found match: " + foundListing.topic);
-//             // get current user id to add to bid object
-//             db.User.findById(req.user, function(err, user) {
-//               if (err) {
-//                 console.log(err);
-//               }
-//               var newBid = new db.Bid(req.body); // add data validation later
-//               newBid.uid = user._id
-//               // pushes new bid object into embedded listing
-//               users[i].listings[j].bids.push(newBid);
-//               users[i].listings[j].save(function(err, savedBid) {
-//                 console.log('newBid created: ', newBid);
-//                 console.log(users[i].listings[j]);
-//                 res.json(newBid);
-//               });
-//             });
-//           }
-//         }
-//       }
-//     })
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// };
-
-
 
 module.exports = {
   index: index,
